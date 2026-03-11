7c3cbccbc2b218040cc0dea1ae7ca3414e3f0fa6
PRISSET Noah <prisset.noah@gmail.com>
2026-03-11 12:44:57 +0100

    feat(database): add TypeORM for database management and initialize connection
    - Added TypeORM as a dependency in package.json.
    - Created a new database.ts file to configure the MySQL data source.
    - Updated index.ts to initialize the database connection on server start.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import { initHandler } from "./handlers/routes.js";
+import { AppDataSource } from "./database/database.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.use(express.json());
 
 initHandler(app);
+
+try {
+  await AppDataSource.initialize();
+} catch (error) {
+  console.log(error);
+  console.log("Failed to initialise database");
+  process.exit(1);
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
